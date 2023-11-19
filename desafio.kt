637c156bf8c03b19a0b865d9b67394c752164990
Lukaz Rocha <40191074+lukazrocha@users.noreply.github.com>
2023-11-19 14:47:23 -0300

    Finalizando Desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,66 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel(val value: String) { 
+    BASICO("Básico"), 
+    INTERMEDIARIO("Intermediário"), 
+    AVANCADO("Avançado")
+}
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(
+    var nome: String
+)
 
-class Usuario
+data class ConteudoEducacional(
+    var nome: String,
+    var nivel: Nivel,
+    val duracao: Int = 60
+)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(
+    val nome: String, 
+    var conteudos: List<ConteudoEducacional>
+) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    
+    fun listarInscritos(): List<String> {
+        return inscritos.map { it.nome }
+    }
+    
+    fun listarConteudos(): List<String> {
+        return conteudos.map {
+            it.nome + " - " + it.nivel.value + " (" + it.duracao + "h)"
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val jake = Usuario("Jake")
+    val finn = Usuario("Finn")
+    
+    val conteudoA = ConteudoEducacional("Kotlin basics", Nivel.BASICO, 20)
+    val conteudoB = ConteudoEducacional("Kotlin functions", Nivel.INTERMEDIARIO, 40)
+    val conteudoC = ConteudoEducacional("Kotlin special topics", Nivel.AVANCADO)
+    
+    val formacaoKotlin = Formacao(
+        "Kotlin DIO", 
+        listOf(
+            conteudoA, 
+            conteudoB, 
+            conteudoC
+        )
+    )
+    
+    formacaoKotlin.matricular(jake)
+    formacaoKotlin.matricular(finn)
+    
+    formacaoKotlin.run {
+        println("Inscritos na Formação $nome: " + listarInscritos())
+        println("Conteúdos: " + listarConteudos())
+    }
+    
+    
 }
